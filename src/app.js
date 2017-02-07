34dc279923afc8a5037d250cbe9b25c736f2cea4
Peng-Jie <dean.leehom@gmail.com>
2017-02-07 09:59:15 +0800

    Add issue report
    remove NODE_ENV and APP_URL

--- src/app.js
+++ src/app.js
@@ -95,6 +95,13 @@ bot.onText(/\location/, (message, match) => {
   }));
 });
 
+bot.onText(/\issue/, message => {
+  const chatId = message.chat.id;
+  const text = '✉️ *Telegram Bot 問題回報:* https://github.com/neighborhood999/telegram-cafe-nomad/issues';
+
+  return bot.sendMessage(chatId, text, options);
+});
+
 bot.on('callback_query', message => {
   switch (message.data) {
     case 'more':
